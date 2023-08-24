a508efce7274cad590b5676debabd7f07e483a8d
9898s <sueh9898@icloud.com>
2023-08-24 23:04:17 +0900

    ErrorCode 내용 추가

--- src/main/java/com/example/kiosk/global/type/ErrorCode.java
+++ src/main/java/com/example/kiosk/global/type/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR("내부 서버에 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다."),
     EXIST_ACCOUNT_EMAIL("이미 가입된 이메일입니다."),
-    NOT_FOUND_ID("찾을 수 없는 아이디 번호입니다.")
+    NOT_FOUND_ID("찾을 수 없는 아이디 번호입니다."),
+    NOT_RESERVE_TIME("예약할 수 없는 시간입니다."),
+    NOT_ARRIVE_TIME("도착 처리할 수 없는 시간입니다."),
+    NOT_CANCEL_TIME("취소 처리 할 수 없는 시간입니다.")
     ;
 
     private String description;
